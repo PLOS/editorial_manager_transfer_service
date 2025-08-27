5b243c5b1305b4aa69ff1f73cc8fe048fe8c5409
Rose Reatherford <rreatherford@plos.org>
2025-08-27 13:04:41 -0500

    Most changes completed minus final task to create article page and integrate with XML transformations from RCT-173.

--- file_creation.py
+++ file_creation.py
@@ -7,22 +7,32 @@ __maintainer__ = "The Public Library of Science (PLOS)"
 
 import os
 import uuid
-import zipfile
 import xml.etree.cElementTree as ET
+import zipfile
 from collections.abc import Sequence
 from typing import List
 
-from journal.models import Journal
-from utils.logger import get_logger
-from utils import setting_handler
 import logger_messages
-
-from submission.models import Article
 from core.models import File
-
+from journal.models import Journal
 from plugins.editorial_manager_transfer_service.plugin_settings import EXPORT_FILE_PATH
+from submission.models import Article
+from utils import setting_handler
+from utils.logger import get_logger
 
 logger = get_logger(__name__)
+
+
+def create_export_files(article_id: str, journal: Journal) -> List[str]:
+    """
+    Returns a list of file paths to the files to be exported.
+    :param article_id: The id of the article.
+    :param journal: The journal where the article is located.
+    :return: A list of file paths to the paths to be exported.
+    """
+    file_creator: FileCreation = FileCreation(article_id, journal)
+
+    return List([file_creator.get_zip_filepath(), file_creator.get_go_filepath()])
 
 
 def get_article_export_folders() -> List[str]:
@@ -38,29 +48,49 @@ def get_article_export_folders() -> List[str]:
 
 
 class FileCreation:
-    def __init__(self, journal: Journal):
-        self.zip_filepath = None
-        self.go_filepath = None
+    """
+    A class for managing the export file creation process.
+    """
+
+    def __init__(self, article_id: str, journal: Journal):
+        self.zip_filepath: str | None = None
+        self.go_filepath: str | None = None
         self.license_code: str = setting_handler.get_setting(
-                setting_group_name="plugin:editorial_manager_transfer_service",
-                setting_name="license_code",
-                journal=journal,
-            ).processed_value
+            setting_group_name="plugin:editorial_manager_transfer_service", setting_name="license_code",
+            journal=journal, ).processed_value
         self.journal_code: str = setting_handler.get_setting(
-            setting_group_name="plugin:editorial_manager_transfer_service",
-            setting_name="journal_code",
-            journal=journal,
-        ).processed_value
+            setting_group_name="plugin:editorial_manager_transfer_service", setting_name="journal_code",
+            journal=journal, ).processed_value
         self.submission_partner_code: str = setting_handler.get_setting(
-            setting_group_name="plugin:editorial_manager_transfer_service",
-            setting_name="submission_partner_code",
-            journal=journal,
-        ).processed_value
+            setting_group_name="plugin:editorial_manager_transfer_service", setting_name="submission_partner_code",
+            journal=journal, ).processed_value
 
         export_folders: Sequence[str] = get_article_export_folders()
         self.export_folder: str | None = export_folders[0] if len(export_folders) > 0 else None
 
-    def create_export_file(self, article_id: str) -> str:
+        self.__create_export_file(article_id)
+
+    def get_zip_filepath(self) -> str | None:
+        """
+        Gets the zip file path for the exported files.
+        :return: The zip file path.
+        """
+        if self.zip_filepath is None:
+            return None
+        else:
+            return self.zip_filepath
+
+    def get_go_filepath(self) -> str | None:
+        """
+        Gets the filepath for the go.xml file for exporting to Editorial Manager.
+        :return: The filepath for the go.xml file or None, if the process failed.
+        """
+        if self.go_filepath is None:
+            return None
+        else:
+            return self.go_filepath
+
+    def __create_export_file(self, article_id: str):
         """
         Creates the export file for
         :param article_id: The ID of the article to create an export file for.
@@ -73,10 +103,19 @@ class FileCreation:
             article: Article = self.__fetch_article(article_id)
         except Exception:
             logger.error(logger_messages.process_failed_fetching_article(article_id))
-            return ""
+            return
 
-        metadata_file: File = self.__create_metadata_file(article)
-        article_files: List[File] = fetch_article_files(article)
+        # Attempt to create the metadata file.
+        metadata_file: File | None = self.__create_metadata_file(article)
+        if metadata_file is None:
+            logger.error(logger_messages.process_failed_fetching_metadata(article_id))
+            return
+
+        # Attempt to fetch the article files.
+        article_files: Sequence[File] = fetch_article_files(article)
+        if len(article_files) <= 0:
+            logger.error(logger_messages.process_failed_fetching_article_files(article_id))
+            return
 
         prefix: str = "{0}_{1}".format(self.submission_partner_code, uuid.uuid4())
 
@@ -90,12 +129,17 @@ class FileCreation:
 
         self.__create_go_xml_file(metadata_file.uuid_filename, filenames, prefix)
 
-        return self.zip_filepath
-
-    def __create_go_xml_file(self, metadata_filename: str, article_filenames: Sequence[str], filename: str) -> None:
+    def __create_go_xml_file(self, metadata_filename: str, article_filenames: Sequence[str], filename: str):
+        """
+        Creates the go xml file for the export process for Editorial Manager.
+        :param metadata_filename: The name of the metadata file.
+        :param article_filenames: The filenames of the article's associated files.
+        :param filename: The name to use for the go.xml file (Must match the name of the zip file).
+        """
         go: ET.Element = ET.Element("GO")
         go.set("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
-        go.set("xsi:noNamespaceSchemaLocation", "app://Aries.EditorialManager/Resources/XmlDefineTransformFiles/aries_import_go_file.xsd")
+        go.set("xsi:noNamespaceSchemaLocation",
+               "app://Aries.EditorialManager/Resources/XmlDefineTransformFiles/aries_import_go_file.xsd")
 
         # Format the header.
         header: ET.Element = ET.SubElement(go, "header")
@@ -103,7 +147,8 @@ class FileCreation:
         ET.SubElement(header, "journal", code=self.journal_code)
         ET.SubElement(header, "import-type", id="2")
         parameters: ET.Element = ET.SubElement(header, "parameters")
-        ET.SubElement(parameters, "parameter", name="license-code", value="{0}_{1}".format(self.submission_partner_code, self.license_code))
+        ET.SubElement(parameters, "parameter", name="license-code",
+                      value="{0}_{1}".format(self.submission_partner_code, self.license_code))
 
         # Begin the filegroup.
         filegroup: ET.Element = ET.SubElement(go, "filegroup")
@@ -119,11 +164,16 @@ class FileCreation:
         self.go_filepath = os.path.join(self.export_folder, "{0}.go.xml".format(filename));
         tree.write(self.go_filepath)
 
-    def __fetch_article(self, article_id: str) -> Article:
-        # TODO: Finish method.
-        pass
+    @staticmethod
+    def __fetch_article(article_id: str) -> Article:
+        """
+        Gets the article object for the given article ID.
+        :param article_id: The ID of the article.
+        :return: The article object with the given article ID.
+        """
+        return Article.get_article(article_id)
 
-    def __create_metadata_file(self, article: Article) -> File:
+    def __create_metadata_file(self, article: Article) -> File | None:
         """
         Creates the metadata file based on the given article.
         :param article: The article to convert to JATS.
@@ -131,32 +181,26 @@ class FileCreation:
         """
         pass
 
+    @staticmethod
+    def fetch_article_files(article: Article) -> List[File]:
+        """
+        Fetches the manuscript (or content or body) of an article alongside any other files associated with it.
+        :param article: The article to fetch the manuscript files for.
+        :return: A list of all files related to the article.
+        """
 
-def fetch_article_files(article: Article) -> List[File]:
-    """
-    Fetches the manuscript (or content or body) of an article alongside any other files associated with it.
-    :param article: The article to fetch the manuscript files for.
-    :return: A list of all files related to the article.
-    """
+        files: List[File] = list()
 
-    files: List[File] = list()
+        for manuscript in article.manuscript_files.all():
+            files.append(manuscript)
 
-    for manuscript in article.manuscript_files.all():
-        files.append(manuscript)
+        for data_file in article.data_figure_files.all():
+            files.append(data_file)
 
-    for data_file in article.data_figure_files.all():
-        files.append(data_file)
+        for source_file in article.source_files.all():
+            files.append(source_file)
 
-    for source_file in article.source_files.all():
-        files.append(source_file)
+        for supplementary_file in article.supplementary_files.all():
+            files.append(supplementary_file)
 
-    for supplementary_file in article.supplementary_files.all():
-        files.append(supplementary_file)
-
-    return files
-
-
-
-
-
-
+        return files
--- logger_messages.py
+++ logger_messages.py
@@ -7,12 +7,14 @@ __maintainer__ = "The Public Library of Science (PLOS)"
 
 from plugins.editorial_manager_transfer_service.plugin_settings import PLUGIN_NAME, EXPORT_FILE_PATH, IMPORT_FILE_PATH
 
+
 def plugin_installation_beginning() -> str:
     """
     Gets the log message for a plugin beginning installation.
     :return: The logger message.
     """
     return '{0} installation beginning...'.format(PLUGIN_NAME)
+
 
 def plugin_installed() -> str:
     """
@@ -21,12 +23,14 @@ def plugin_installed() -> str:
     """
     return '{0} installed.'.format(PLUGIN_NAME)
 
+
 def plugin_already_installed() -> str:
     """
     Gets the log message for a plugin has been previously installed.
     :return: The logger message.
     """
     return '{0} is already installed.'.format(PLUGIN_NAME)
+
 
 def export_folder_creating() -> str:
     """
@@ -35,12 +39,14 @@ def export_folder_creating() -> str:
     """
     return '{0} creating export folder (Filepath: \"{1}\")...'.format(PLUGIN_NAME, EXPORT_FILE_PATH)
 
+
 def export_folder_created() -> str:
     """
     Gets the log message for when an export folder has been created.
     :return: The logger message.
     """
     return '{0} export folder already exists.'.format(PLUGIN_NAME)
+
 
 def import_folder_creating() -> str:
     """
@@ -49,12 +55,14 @@ def import_folder_creating() -> str:
     """
     return '{0} creating import folder (Filepath: \"{1}\")...'.format(PLUGIN_NAME, IMPORT_FILE_PATH)
 
+
 def import_folder_created() -> str:
     """
     Gets the log message for when an import folder has been created.
     :return: The logger message.
     """
     return '{0} import folder already exists.'.format(PLUGIN_NAME)
+
 
 def process_fetching_article(article_id: str) -> str:
     """
@@ -64,10 +72,29 @@ def process_fetching_article(article_id: str) -> str:
     """
     return "Fetching article from database (ID: {0})...".format(article_id)
 
+
 def process_failed_fetching_article(article_id: str) -> str:
     """
     Gets the log message for when an article failed to be fetched.
     :param: article_id: The ID of the article being fetched.
     :return: The logger message.
     """
-    return "Fetching article from database (ID: {0})...".format(article_id)
+    return "Fetching article from database (ID: {0}) failed. Discontinuing export process.".format(article_id)
+
+
+def process_failed_fetching_metadata(article_id):
+    """
+    Gets the log message for when an article failed to be fetched.
+    :param: article_id: The ID of the article being fetched.
+    :return: The logger message.
+    """
+    return "Fetching article (ID: {0}) metadata failed. Discontinuing export process.".format(article_id)
+
+
+def process_failed_fetching_article_files(article_id):
+    """
+    Gets the log message for when an article failed to be fetched.
+    :param: article_id: The ID of the article being fetched.
+    :return: The logger message.
+    """
+    return "Fetching files for article (ID: {0}) failed. Discontinuing export process.".format(article_id)
