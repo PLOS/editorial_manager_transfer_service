f1d82ea89ccfdfc42109f68756c18bd787538117
Joshua Lavarine <jlavarine@plos.org>
2025-10-13 14:01:43 +0000

    Basic sel

--- tests/integration/test_selenium.py
+++ tests/integration/test_selenium.py
@@ -1,10 +1,21 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import unittest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import chromedriver_autoinstaller
 
-class MySeleniumTests(StaticLiveServerTestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
+class BasicSeleniumTest(unittest.TestCase):
+    def setUp(self):
+        chromedriver_autoinstaller.install()
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        self.driver = webdriver.Chrome(options=chrome_options)
 
-    def test_login_page(self):
-        self.assertTrue(True)
- 
+    def test_homepage_title(self):
+        self.driver.get("http://localhost:8000")  # Adjust URL if needed
+        self.assertIn("Janeway", self.driver.title)
+
+    def tearDown(self):
+        self.driver.quit()
