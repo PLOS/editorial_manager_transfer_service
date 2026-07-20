b93f96d173a08e40e7342b59f3bf9680103b2451
Rose Reatherford <rreatherford@plos.org>
2026-07-20 11:58:01 -0500

    Add extra logging to check for errors.

--- file_exporter.py
+++ file_exporter.py
@@ -1,6 +1,7 @@
 """
 A list of functions handling file exports and imports.
 """
+
 __author__ = "Rosetta Reatherford"
 __license__ = "AGPL v3"
 __maintainer__ = "The Public Library of Science (PLOS)"
@@ -18,22 +19,34 @@ import plugins.editorial_manager_transfer_service.logger_messages as logger_mess
 from core.models import File
 from journal.models import Journal
 from plugins.editorial_manager_transfer_service.enums.report_state import ReportState
-from plugins.editorial_manager_transfer_service.enums.transfer_log_message_type import TransferLogMessageType
+from plugins.editorial_manager_transfer_service.enums.transfer_log_message_type import (
+    TransferLogMessageType,
+)
 from plugins.editorial_manager_transfer_service.models import TransferLogs
-from plugins.editorial_manager_transfer_service.utils.jats import get_xml_license_code, generate_jats_metadata
-from plugins.editorial_manager_transfer_service.utils.settings import get_license_code, get_submission_partner_code, \
-    get_journal_code
-from plugins.editorial_manager_transfer_service.utils.transfer_report import get_or_create_transfer_report, \
-    resolve_transfer_report
+from plugins.editorial_manager_transfer_service.utils.jats import (
+    get_xml_license_code,
+    generate_jats_metadata,
+)
+from plugins.editorial_manager_transfer_service.utils.settings import (
+    get_license_code,
+    get_submission_partner_code,
+    get_journal_code,
+)
+from plugins.editorial_manager_transfer_service.utils.transfer_report import (
+    get_or_create_transfer_report,
+    resolve_transfer_report,
+)
 from plugins.production_transporter.utilities import data_fetch
-from plugins.production_transporter.utilities.file_utils import copy_files_to_temp_deposit_folder
+from plugins.production_transporter.utilities.file_utils import (
+    copy_files_to_temp_deposit_folder,
+)
 from submission.models import Article
 from utils.logger import get_logger
 
 logger = get_logger(__name__)
 
 
-def get_article_export_folders() -> str:
+def get_article_export_folders() -> str | None:
     """
     Gets the filepaths for the folders used for exporting articles.
 
@@ -42,7 +55,10 @@ def get_article_export_folders() -> str:
     if os.path.exists(consts.EXPORT_FILE_PATH):
         return consts.EXPORT_FILE_PATH
     else:
-        return ""
+        logger.warn(
+            f"No export file was found. The given export folder was: {consts.EXPORT_FILE_PATH}"
+        )
+        return None
 
 
 class ExportFileCreation:
@@ -60,7 +76,7 @@ class ExportFileCreation:
         self.article_id: int | None = article_id
         self.article: Article | None = None
         self.journal: Journal | None = None
-        self.export_folder: str | None = None
+        self.export_folder: str = ""
         self.xml_filepath: str | None = None
         self.__temp_folder: str | None = None
 
@@ -75,8 +91,8 @@ class ExportFileCreation:
             return
 
         # Get the export folder.
-        export_folders: str = get_article_export_folders()
-        if len(export_folders) <= 0:
+        export_folders: str | None = get_article_export_folders()
+        if export_folders is None:
             self.log_error(logger_messages.export_process_failed_no_export_folder())
             self.in_error_state = True
             return
@@ -121,20 +137,26 @@ class ExportFileCreation:
 
         prefix: str = "{0}_{1}".format(self.get_submission_partner_code(), uuid.uuid4())
 
-        self.zip_filepath: str = os.path.join(self.export_folder, "{0}.zip".format(prefix))
+        self.zip_filepath: str = os.path.join(
+            self.export_folder, "{0}.zip".format(prefix)
+        )
         self.__temp_folder = os.path.join(self.export_folder, "{0}".format(prefix))
         os.makedirs(self.__temp_folder, exist_ok=True)
 
         # Attempt to get the metadata file.
         if self.__get_xml_filepath() is None:
-            logger.error(logger_messages.process_failed_fetching_metadata(self.article_id))
+            logger.error(
+                logger_messages.process_failed_fetching_metadata(self.article_id)
+            )
             self.in_error_state = True
             return
 
         # Attempt to fetch the article files.
         article_files: Sequence[File] = self.__fetch_article_files(self.article)
         if len(article_files) <= 0:
-            self.log_error(logger_messages.process_failed_fetching_article_files(self.article_id))
+            self.log_error(
+                logger_messages.process_failed_fetching_article_files(self.article_id)
+            )
             self.in_error_state = True
             return
 
@@ -149,7 +171,9 @@ class ExportFileCreation:
         deposit_helpers.zip_temp_folder(temp_folder=self.__temp_folder)
 
         # Remove the manuscript
-        self.__create_go_xml_file(os.path.basename(self.__get_xml_filepath()), filenames, prefix)
+        self.__create_go_xml_file(
+            os.path.basename(self.__get_xml_filepath()), filenames, prefix
+        )
 
     def get_license_code(self) -> str:
         """
@@ -175,7 +199,9 @@ class ExportFileCreation:
         :return: The submission partner code or None, if the process failed.
         """
         if not self.__submission_partner_code:
-            self.__submission_partner_code: str = get_submission_partner_code(self.journal)
+            self.__submission_partner_code: str = get_submission_partner_code(
+                self.journal
+            )
         return self.__submission_partner_code
 
     def can_export(self) -> bool:
@@ -183,14 +209,18 @@ class ExportFileCreation:
         Checks if the export file can be created.
         :return: True if the export file can be created, False otherwise.
         """
-        return (not self.in_error_state and
-                self.article is not None and
-                self.journal is not None and
-                self.get_license_code() is not None and
-                self.get_journal_code() is not None and
-                self.get_submission_partner_code() is not None)
+        return (
+            not self.in_error_state
+            and self.article is not None
+            and self.journal is not None
+            and self.get_license_code() is not None
+            and self.get_journal_code() is not None
+            and self.get_submission_partner_code() is not None
+        )
 
-    def __create_go_xml_file(self, metadata_filename: str, article_filenames: Sequence[str], filename: str):
+    def __create_go_xml_file(
+        self, metadata_filename: str, article_filenames: Sequence[str], filename: str
+    ):
         """
         Creates the go xml file for the export process for Editorial Manager.
         :param metadata_filename: The name of the metadata file.
@@ -202,32 +232,67 @@ class ExportFileCreation:
             return
 
         go: ETree.Element = ETree.Element(consts.GO_FILE_ELEMENT_TAG_GO)
-        go.set(consts.GO_FILE_GO_ELEMENT_ATTRIBUTE_XMLNS_XSI_KEY, consts.GO_FILE_GO_ELEMENT_ATTRIBUTE_XMLNS_XSI_VALUE)
-        go.set(consts.GO_FILE_GO_ELEMENT_ATTRIBUTE_SCHEMA_LOCATION_KEY,
-               consts.GO_FILE_GO_ELEMENT_ATTRIBUTE_SCHEMA_LOCATION_VALUE)
+        go.set(
+            consts.GO_FILE_GO_ELEMENT_ATTRIBUTE_XMLNS_XSI_KEY,
+            consts.GO_FILE_GO_ELEMENT_ATTRIBUTE_XMLNS_XSI_VALUE,
+        )
+        go.set(
+            consts.GO_FILE_GO_ELEMENT_ATTRIBUTE_SCHEMA_LOCATION_KEY,
+            consts.GO_FILE_GO_ELEMENT_ATTRIBUTE_SCHEMA_LOCATION_VALUE,
+        )
 
         # Format the header.
         header: ETree.Element = ETree.SubElement(go, consts.GO_FILE_ELEMENT_TAG_HEADER)
-        version: ETree.Element = ETree.SubElement(header, consts.GO_FILE_ELEMENT_TAG_VERSION)
-        version.set(consts.GO_FILE_VERSION_ELEMENT_ATTRIBUTE_NUMBER_KEY,
-                    consts.GO_FILE_VERSION_ELEMENT_ATTRIBUTE_NUMBER_VALUE)
-        journal: ETree.Element = ETree.SubElement(header, consts.GO_FILE_ELEMENT_TAG_JOURNAL)
-        journal.set(consts.GO_FILE_JOURNAL_ELEMENT_ATTRIBUTE_CODE_KEY, self.get_journal_code())
-        import_type: ETree.Element = ETree.SubElement(header, consts.GO_FILE_ELEMENT_TAG_IMPORT_TYPE)
-        import_type.set(consts.GO_FILE_IMPORT_TYPE_ELEMENT_ATTRIBUTE_ID_KEY,
-                        consts.GO_FILE_IMPORT_TYPE_ELEMENT_ATTRIBUTE_ID_VALUE)
-        parameters: ETree.Element = ETree.SubElement(header, consts.GO_FILE_ELEMENT_TAG_PARAMETERS)
-        parameter: ETree.Element = ETree.SubElement(parameters, consts.GO_FILE_ELEMENT_TAG_PARAMETER)
-        parameter.set(consts.GO_FILE_ATTRIBUTE_ELEMENT_NAME_KEY, consts.GO_FILE_PARAMETER_ELEMENT_NAME_VALUE)
-        parameter.set(consts.GO_FILE_PARAMETER_ELEMENT_VALUE_KEY, get_xml_license_code(self.journal))
+        version: ETree.Element = ETree.SubElement(
+            header, consts.GO_FILE_ELEMENT_TAG_VERSION
+        )
+        version.set(
+            consts.GO_FILE_VERSION_ELEMENT_ATTRIBUTE_NUMBER_KEY,
+            consts.GO_FILE_VERSION_ELEMENT_ATTRIBUTE_NUMBER_VALUE,
+        )
+        journal: ETree.Element = ETree.SubElement(
+            header, consts.GO_FILE_ELEMENT_TAG_JOURNAL
+        )
+        journal.set(
+            consts.GO_FILE_JOURNAL_ELEMENT_ATTRIBUTE_CODE_KEY, self.get_journal_code()
+        )
+        import_type: ETree.Element = ETree.SubElement(
+            header, consts.GO_FILE_ELEMENT_TAG_IMPORT_TYPE
+        )
+        import_type.set(
+            consts.GO_FILE_IMPORT_TYPE_ELEMENT_ATTRIBUTE_ID_KEY,
+            consts.GO_FILE_IMPORT_TYPE_ELEMENT_ATTRIBUTE_ID_VALUE,
+        )
+        parameters: ETree.Element = ETree.SubElement(
+            header, consts.GO_FILE_ELEMENT_TAG_PARAMETERS
+        )
+        parameter: ETree.Element = ETree.SubElement(
+            parameters, consts.GO_FILE_ELEMENT_TAG_PARAMETER
+        )
+        parameter.set(
+            consts.GO_FILE_ATTRIBUTE_ELEMENT_NAME_KEY,
+            consts.GO_FILE_PARAMETER_ELEMENT_NAME_VALUE,
+        )
+        parameter.set(
+            consts.GO_FILE_PARAMETER_ELEMENT_VALUE_KEY,
+            get_xml_license_code(self.journal),
+        )
 
         # Begin the filegroup.
-        filegroup: ETree.Element = ETree.SubElement(go, consts.GO_FILE_ELEMENT_TAG_FILEGROUP)
+        filegroup: ETree.Element = ETree.SubElement(
+            go, consts.GO_FILE_ELEMENT_TAG_FILEGROUP
+        )
 
         # Create the archive and metadata files.
-        archive_file: ETree.Element = ETree.SubElement(filegroup, consts.GO_FILE_ELEMENT_TAG_ARCHIVE_FILE)
-        archive_file.set(consts.GO_FILE_ATTRIBUTE_ELEMENT_NAME_KEY, "{0}.zip".format(filename))
-        metadata_file: ETree.Element = ETree.SubElement(filegroup, consts.GO_FILE_ELEMENT_TAG_METADATA_FILE)
+        archive_file: ETree.Element = ETree.SubElement(
+            filegroup, consts.GO_FILE_ELEMENT_TAG_ARCHIVE_FILE
+        )
+        archive_file.set(
+            consts.GO_FILE_ATTRIBUTE_ELEMENT_NAME_KEY, "{0}.zip".format(filename)
+        )
+        metadata_file: ETree.Element = ETree.SubElement(
+            filegroup, consts.GO_FILE_ELEMENT_TAG_METADATA_FILE
+        )
         metadata_file.set(consts.GO_FILE_ATTRIBUTE_ELEMENT_NAME_KEY, metadata_filename)
 
         for article_filename in article_filenames:
@@ -235,7 +300,9 @@ class ExportFileCreation:
             file_tree.set(consts.GO_FILE_ATTRIBUTE_ELEMENT_NAME_KEY, article_filename)
 
         tree = ETree.ElementTree(go)
-        self.go_filepath = os.path.join(self.export_folder, "{0}.go.xml".format(filename))
+        self.go_filepath = os.path.join(
+            self.export_folder, "{0}.go.xml".format(filename)
+        )
         tree.write(self.go_filepath)
 
     def __get_xml_filepath(self) -> str | None:
@@ -244,7 +311,9 @@ class ExportFileCreation:
         :return:The filepath to the JATS XML file.
         """
         if not self.xml_filepath:
-            filepath = generate_jats_metadata(self.journal, self.article, self.__temp_folder)
+            filepath = generate_jats_metadata(
+                self.journal, self.article, self.__temp_folder
+            )
 
             if not filepath:
                 self.in_error_state = True
@@ -253,7 +322,9 @@ class ExportFileCreation:
             self.xml_filepath = filepath
         return self.xml_filepath
 
-    def __fetch_article(self, journal: Journal | None, article_id: int | None) -> Article | None:
+    def __fetch_article(
+        self, journal: Journal | None, article_id: int | None
+    ) -> Article | None:
         """
         Gets the article object for the given article ID.
         :param journal: The journal to fetch the article from.
@@ -265,6 +336,7 @@ class ExportFileCreation:
         article: Article | None = data_fetch.fetch_article(journal, article_id)
         if article is None:
             self.in_error_state = True
+            logger.warn(f"Could not fetch article (ID: {article_id}).")
             return None
 
         logger.debug(logger_messages.process_finished_fetching_article(article_id))
@@ -284,12 +356,18 @@ class ExportFileCreation:
             self.in_error_state = True
             return None
 
-        logger.debug(logger_messages.process_finished_fetching_journal(janeway_journal_code))
+        logger.debug(
+            logger_messages.process_finished_fetching_journal(janeway_journal_code)
+        )
 
         return journal
 
-    def log_error(self, message: str, error: Exception = None,
-                  stage: ReportState = ReportState.FAILED_BUNDLING) -> None:
+    def log_error(
+        self,
+        message: str,
+        error: Exception = None,
+        stage: ReportState = ReportState.FAILED_BUNDLING,
+    ) -> None:
         """
         Logs the given error message in both the database and plaintext logs.
         :param message: The message to log.
@@ -301,25 +379,40 @@ class ExportFileCreation:
         if self.transfer_report.report_state != stage:
             self.transfer_report.report_state = stage
             self.transfer_report.save()
-        TransferLogs.objects.create(report=self.transfer_report, journal=self.journal, article=self.article,
-                                    message=message,
-                                    message_type=TransferLogMessageType.EXPORT, success=False)
+        TransferLogs.objects.create(
+            report=self.transfer_report,
+            journal=self.journal,
+            article=self.article,
+            message=message,
+            message_type=TransferLogMessageType.EXPORT,
+            success=False,
+        )
 
     def log_success_go_file(self) -> None:
         """
         Logs a success message in both the database and plaintext logs.
         """
-        TransferLogs.objects.create(report=self.transfer_report, journal=self.journal, article=self.article,
-                                    message=logger_messages.export_go_file_process_succeeded(self.article_id),
-                                    message_type=TransferLogMessageType.EXPORT, success=True)
+        TransferLogs.objects.create(
+            report=self.transfer_report,
+            journal=self.journal,
+            article=self.article,
+            message=logger_messages.export_go_file_process_succeeded(self.article_id),
+            message_type=TransferLogMessageType.EXPORT,
+            success=True,
+        )
 
     def log_success_zip_file(self) -> None:
         """
         Logs a success message in both the database and plaintext logs.
         """
-        TransferLogs.objects.create(report=self.transfer_report, journal=self.journal, article=self.article,
-                                    message=logger_messages.export_zip_file_process_succeeded(self.article_id),
-                                    message_type=TransferLogMessageType.EXPORT, success=True)
+        TransferLogs.objects.create(
+            report=self.transfer_report,
+            journal=self.journal,
+            article=self.article,
+            message=logger_messages.export_zip_file_process_succeeded(self.article_id),
+            message_type=TransferLogMessageType.EXPORT,
+            success=True,
+        )
         resolve_transfer_report(self.transfer_report)
 
     @staticmethod
