4ef7bdd2bbf2f851bade7733435517782cc6fa71
Rose Reatherford <rreatherford@plos.org>
2026-07-20 11:26:46 -0500

    Update export and import folder filepath.

--- consts.py
+++ consts.py
@@ -7,13 +7,13 @@ import os
 from django.conf import settings
 
 # Plugin Settings
-PLUGIN_NAME = 'Editorial Manager Transfer Service Plugin'
-DISPLAY_NAME = 'Editorial Manager Transfer Service'
-DESCRIPTION = 'A plugin to provide information for Aries\' Editorial Manager to enable automatic transfers.'
-AUTHOR = 'PLOS'
-VERSION = '0.1'
-SHORT_NAME = 'editorial_manager_transfer_service'
-MANAGER_URL = 'editorial_manager_transfer_service_manager'
+PLUGIN_NAME = "Editorial Manager Transfer Service Plugin"
+DISPLAY_NAME = "Editorial Manager Transfer Service"
+DESCRIPTION = "A plugin to provide information for Aries' Editorial Manager to enable automatic transfers."
+AUTHOR = "PLOS"
+VERSION = "0.1"
+SHORT_NAME = "editorial_manager_transfer_service"
+MANAGER_URL = "editorial_manager_transfer_service_manager"
 JANEWAY_VERSION = "1.8.0"
 
 # Setting Configuration
@@ -23,13 +23,27 @@ PLUGIN_SETTINGS_JOURNAL_CODE = "journal_code"
 PLUGIN_SETTINGS_SUBMISSION_PARTNER_CODE = "submission_partner_code"
 
 # Import and export filepaths
-EXPORT_FILE_PATH = os.path.join(settings.BASE_DIR, 'files', 'plugins', 'editorial-manager-transfer-service', 'export')
-IMPORT_FILE_PATH = os.path.join(settings.BASE_DIR, 'files', 'plugins', 'editorial-manager-transfer-service', 'import')
+EXPORT_FILE_PATH = os.path.join(
+    settings.BASE_DIR,
+    "files",
+    "plugins",
+    SHORT_NAME,
+    "export",
+)
+IMPORT_FILE_PATH = os.path.join(
+    settings.BASE_DIR,
+    "files",
+    "plugins",
+    SHORT_NAME,
+    "import",
+)
 
 # XML File
 GO_FILE_ELEMENT_TAG_GO = "GO"
 GO_FILE_GO_ELEMENT_ATTRIBUTE_XMLNS_XSI_KEY = "xmlns:xsi"
-GO_FILE_GO_ELEMENT_ATTRIBUTE_XMLNS_XSI_VALUE = "http://www.w3.org/2001/XMLSchema-instance"
+GO_FILE_GO_ELEMENT_ATTRIBUTE_XMLNS_XSI_VALUE = (
+    "http://www.w3.org/2001/XMLSchema-instance"
+)
 GO_FILE_GO_ELEMENT_ATTRIBUTE_SCHEMA_LOCATION_KEY = "xsi:noNamespaceSchemaLocation"
 GO_FILE_GO_ELEMENT_ATTRIBUTE_SCHEMA_LOCATION_VALUE = "app://Aries.EditorialManager/Resources/XmlDefineTransformFiles/aries_import_go_file.xsd"
 GO_FILE_ELEMENT_TAG_HEADER = "header"
@@ -51,4 +65,4 @@ GO_FILE_ELEMENT_TAG_FILEGROUP = "filegroup"
 GO_FILE_ELEMENT_TAG_FILE = "file"
 GO_FILE_ELEMENT_TAG_METADATA_FILE = "metadata-file"
 
-JATS_XML_FILE = 'editorial_manager_transfer_service/encoding/article_jats_1_2_aries.xml'
+JATS_XML_FILE = "editorial_manager_transfer_service/encoding/article_jats_1_2_aries.xml"
